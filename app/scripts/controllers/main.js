bf71a08fe862aa208f7c72fe71692c64c83d6336
awsomer365 <awsomer365@gmail.com>
2015-07-21 14:46:25 -0700

    The First Upload
    First upload of the v-mesa repository, mainly a test for github

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vmBlocks3App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vmBlocks3App
+ */
+angular.module('vmBlocks3App')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+       
+  });
+  
+  
